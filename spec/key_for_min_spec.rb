adf498ac92329decf8ed5332bde2f78853a4a1a9
Eric Loudon <loudon.eric@gmail.com>
2019-09-27 10:47:44 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -64,9 +64,6 @@ describe "smallest hash value" do
     lowest_key = nil
 
     name_hash.each do |key,value|
-      #if the lowest_value is in it's intial state
-      #or if the value of the current key is less than the lowest_value
-      #then: set the lowest_value to value and lowest_key to key
       if lowest_value == 0 || value < lowest_value
         lowest_value = value
         lowest_key = key
@@ -77,4 +74,5 @@ describe "smallest hash value" do
 
   ikea = {:chair => 25, :table => 85, :mattress => 450}
   key_for_min_value(ikea)
+
 end
